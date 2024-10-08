b964997843743855a5df1b76abf99a5a24a8d281
Tetiana Bolyukh <hmontarez@gmail.com>
2024-10-08 11:37:23 +0200

    Sekcja 3 skonczona

---
+++ 02-TypesAndVariables/3-2.py
@@ -0,0 +1,12 @@
+###
+# A program that calculates the sum of two numbers.
+# Modify the program to calculate the sum of three numbers.
+#
+number1 = 71
+number2 = 14
+result = number1 + number2
+Number1 = input('Number 1: ')
+Number2 = input('Number 2: ')
+print('Number 1: ', number1)
+print('Number 2: ', number2)
+print('The result of summation: ', result)
---
+++ 02-TypesAndVariables/3-6.py
@@ -0,0 +1,4 @@
+
+h = int(input("Enter  the distance: ",))
+d = 3.57*
+h = int(input("Enter  the distance "))
