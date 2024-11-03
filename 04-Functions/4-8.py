15aaa056b6e6263e39b8bf801884066852e2dce8
Tetiana Bolyukh <hmontarez@gmail.com>
2024-11-03 15:43:37 +0100

    4-8 changed

--- 04-Functions/4-8.py
+++ 04-Functions/4-8.py
@@ -5,13 +5,23 @@
 # ('24' for 24-hour time and '12' for 12-hour time)
 #
 def time_string(hours, minutes, time_format):
-    if time_format == 24:
+    if time_format == '24':
         return f'{hours:02}:{minutes:02}'
     elif time_format == '12':
         if hours >= 12:
             period = 'pm'
-        else:
             hours = hours - 12
+            return f'{hours:02}:{minutes:02}{period}'
+        else:
             period = 'am'
-        return f'{hours}:{minutes}{period}'
-    time_string(15, 38, 24)
+            return f'{hours:02}:{minutes:02}{period}'
+        
+print(time_string(15, 38, '24'))
+print(time_string(8, 3, '24'))
+print(time_string(0, 5, '24'))
+print(time_string(11, 15, '12'))
+print(time_string(0, 7, '12'))
+print(time_string(7, 30, '12'))
+print(time_string(12, 46, '12'))
+print(time_string(13, 10, '12'))
+print(time_string(19, 2, '12'))
